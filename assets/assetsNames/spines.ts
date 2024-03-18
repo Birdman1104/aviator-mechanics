cce58488f0b43177769dba72f049a663db6255a6
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-03-18 18:58:27 +0000

    Deploying to gh-pages from @ Birdman1104/aviator-mechanics@d77bfd2325fefdf34f4710780690c406b24bb250 🚀

--- assets/assetsNames/spines.ts
+++
@@ -1,8 +0,0 @@
-export const spines: SpineFiles[] = [
-    {
-        key: 'spineboy',
-        jsonURL: 'assets/spines/spineboy/spineboy.json',
-        atlasURL: 'assets/spines/spineboy/spineboy.atlas',
-        preMultipliedAlpha: true,
-    },
-];
