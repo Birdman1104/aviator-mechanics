47e355f44dd3a0ec2be35f264dcaa6f8be9c257d
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-03-18 18:43:12 +0000

    Deploying to gh-pages from @ Birdman1104/aviator-mechanics@e62db1a2074b74385c2fc5402f55daffb0806eb3 🚀

---
+++ assets/assetsNames/atlas.ts
@@ -0,0 +1,3 @@
+export const atlases: { name: string; json: string; png: string }[] = [
+    { name: 'game-ui', json: 'assets/atlas/game-ui@1.png.json', png: 'assets/atlas/game-ui.png' },
+];
---
+++ assets/assetsNames/fonts.ts
@@ -0,0 +1,1 @@
+export const fonts: AssetNameAndPath[] = [{ name: 'Filmotype_Major', path: 'assets/fonts/Filmotype_Major.otf' }];
